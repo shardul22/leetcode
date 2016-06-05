aac6e8c496651f16fdb6aa8a239d666683b74cd0
Yogesh Patwardhan <patwardhan.yogesh16@gmail.com>
2016-06-05 17:06:37 -0500

    Problem-101 : Symmetric Tree

---
+++ src/com/java/tree/SymmetricTree.java
@@ -0,0 +1,24 @@
+package com.java.tree;
+
+public class SymmetricTree {
+	
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) {
+        	return true;
+        }
+        return isMirrorHelper(root.left, root.right);
+    }
+    
+    public boolean isMirrorHelper(TreeNode leftNode, TreeNode rightNode) {
+    	if(leftNode == null && rightNode == null)
+    		return true;
+    	
+    	if(leftNode == null || rightNode == null)
+    		return false;
+    	
+    	return leftNode.val == rightNode.val && 
+    			isMirrorHelper(leftNode.left, rightNode.right) && 
+    			isMirrorHelper(leftNode.right, rightNode.left);
+    }
+
+}
