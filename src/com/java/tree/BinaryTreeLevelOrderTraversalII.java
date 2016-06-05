3e6531fd8296a85338718e6d3fd384006c007b85
Yogesh Patwardhan <patwardhan.yogesh16@gmail.com>
2016-06-05 16:58:28 -0500

    Problem-107 : Binary Tree Level Order Traversal

---
+++ src/com/java/tree/BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,45 @@
+package com.java.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+public class BinaryTreeLevelOrderTraversalII {
+
+	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+		List<List<Integer>> resultList = new ArrayList<>();
+		if(root != null) {
+			Queue<TreeNode> levelQueue = new LinkedList<>();
+			levelQueue.offer(root);
+			levelQueue.offer(null);
+			List<Integer> tempList = new ArrayList<>();
+			Stack<List<Integer>> tempStack = new Stack<>();
+			while(!levelQueue.isEmpty()) {
+				TreeNode temp = levelQueue.poll();
+				if(temp == null) {
+					if(!levelQueue.isEmpty())
+						levelQueue.add(null);
+					tempStack.add(tempList);
+					tempList = new ArrayList<Integer>();
+				} else {
+					tempList.add(temp.val);
+					if(temp.left !=null) {
+						levelQueue.offer(temp.left);
+					}
+					if(temp.right !=null) {
+						levelQueue.offer(temp.right);
+					}
+				}
+			}
+
+			while(!tempStack.isEmpty()) {
+				resultList.add(tempStack.pop());
+			}
+		}
+
+		return resultList;
+	}
+
+}
