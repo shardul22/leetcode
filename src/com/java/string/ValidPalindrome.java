f6f5f7e1319d6deccbeab97342e38ece20eac49a
Yogesh Patwardhan <patwardhan.yogesh16@gmail.com>
2016-06-11 12:54:13 -0500

    Problem-125 : Valid Palindrome

---
+++ src/com/java/string/ValidPalindrome.java
@@ -0,0 +1,41 @@
+package com.java.string;
+
+/*
+ * https://leetcode.com/problems/valid-palindrome/
+ * */
+public class ValidPalindrome {
+
+	public boolean isPalindrome(String s) {
+
+		if(s == null || s == "")
+			return false;
+
+		int i=0, j=s.length()-1;
+		s=s.toLowerCase();
+		while(i<j) {
+			if(isValidCharacter(s.charAt(i)) && isValidCharacter(s.charAt(j))) {
+				if(s.charAt(i) == s.charAt(j)) {
+					i++;
+					j--;
+				} else {
+					return false;
+				}
+			}
+			if(!isValidCharacter(s.charAt(i))) {
+				i++;
+			}
+			if(!isValidCharacter(s.charAt(j))) {
+				j--;
+			}
+
+		}
+		return true;
+	}
+
+	public boolean isValidCharacter(char c) {
+		if((c >= 'a' && c <= 'z') ||(c >= 'A' && c <= 'Z') ||(c >= '0' && c <= '9')) {
+			return true;
+		}
+		return false;
+	}
+}
