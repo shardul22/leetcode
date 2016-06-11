ab9e16f36e45d671f17aa87de351b9ee54bded54
Yogesh Patwardhan <patwardhan.yogesh16@gmail.com>
2016-06-11 08:33:54 -0500

    Problem-171 : Excel Sheet to Column Number

---
+++ src/com/java/math/ExcelSheetColumnNumber.java
@@ -0,0 +1,20 @@
+package com.java.math;
+
+public class ExcelSheetColumnNumber {
+
+	public int titleToNumber(String s) {
+		
+		if(s == null || s.length() == 0)
+			return 0;
+		
+		int length = s.length()-1;
+		int unitPlace = 0, result = 0;
+		
+		while(length >= 0) {
+			result = result + (int) Math.pow(26, unitPlace) * (s.charAt(length)-'A'+1);
+			length--;
+			unitPlace++;
+		}
+		return result;
+	}
+}
