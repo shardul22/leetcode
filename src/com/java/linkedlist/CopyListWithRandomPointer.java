26fa5a5daa927e3878250a6c5402b174f23c2b6d
Yogesh Patwardhan <patwardhan.yogesh16@gmail.com>
2016-06-12 17:02:10 -0500

    Problem-138 : Copy List with Random Pointers

--- src/com/java/linkedlist/CopyListWithRandomPointer.java
+++ src/com/java/linkedlist/CopyListWithRandomPointer.java
@@ -1,10 +1,51 @@
 package com.java.linkedlist;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * https://leetcode.com/problems/copy-list-with-random-pointer/
+ * */
 public class CopyListWithRandomPointer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public RandomListNode copyRandomList(RandomListNode head) {
+		
+		if(head == null)
+			return head;
+		
+		Map<RandomListNode, RandomListNode> nodeMap = new HashMap<>();
+		RandomListNode copyHead = new RandomListNode(head.label);
+		nodeMap.put(head, copyHead);
+		RandomListNode copyHeadRef = copyHead;
+		while(head != null) {
+			if(head.next != null) {
+				if(!nodeMap.containsKey(head.next)) {
+					RandomListNode copyNextPointerNode = new RandomListNode(head.next.label);
+					nodeMap.put(head.next, copyNextPointerNode);
+					copyHeadRef.next = copyNextPointerNode;
+				} else {
+					copyHeadRef.next = nodeMap.get(head.next);
+				}
+			}
+			if(head.random != null) {
+				if(!nodeMap.containsKey(head.next)) {
+					RandomListNode coyRandomPointerNode = new RandomListNode(head.random.label);
+					nodeMap.put(head.next, coyRandomPointerNode);
+					copyHeadRef.next = coyRandomPointerNode;
+				} else {
+					copyHeadRef.random = nodeMap.get(head.random);
+				}
+			}
+			head = head.next;
+			copyHeadRef = copyHeadRef.next;
+		}
+		return copyHead;
 	}
 
+}
+
+class RandomListNode {
+	int label;
+	RandomListNode next, random;
+	RandomListNode(int x) { this.label = x; }
 }
