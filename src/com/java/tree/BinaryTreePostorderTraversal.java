cc799057ac9fb7be08e8e037b673cd1b853f26bd
Yogesh Patwardhan <patwardhan.yogesh16@gmail.com>
2016-07-31 10:47:59 -0500

    Problem 145 : Binary Tree Post Order Traversal

---
+++ src/com/java/tree/BinaryTreePostorderTraversal.java
@@ -0,0 +1,42 @@
+package com.java.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/
+ */
+public class BinaryTreePostorderTraversal {
+
+	public List<Integer> postorderTraversal(TreeNode root) {
+		List<Integer> resultList = new ArrayList<>();
+		if(root == null) {
+			return resultList;
+		}
+		Stack<TreeNode> nodeStack = new Stack<>();
+		while(root != null || !nodeStack.isEmpty()) {
+			if(root != null) {
+				nodeStack.push(root);
+				root = root.left;
+			} else {
+				if(nodeStack.peek().right != null) {
+					root = nodeStack.peek().right;
+				} else {
+					root = nodeStack.pop();
+					resultList.add(root.val);
+					while(!nodeStack.isEmpty() && root == nodeStack.peek().right) {
+						root = nodeStack.pop();
+						resultList.add(root.val);
+					}
+					if(!nodeStack.isEmpty()) {
+						root = nodeStack.peek().right;
+					} else {
+						root = null;
+					}
+				}
+			}
+		}
+		return resultList;
+	}
+}
