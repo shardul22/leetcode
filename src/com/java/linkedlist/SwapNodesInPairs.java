082d80687cae6c5cdbc916149a3053b44df07715
Yogesh Patwardhan <patwardhan.yogesh16@gmail.com>
2016-08-20 21:29:53 -0500

    Problem-24 : SwapNodes in Pairs

--- src/com/java/linkedlist/SwapNodesInPairs.java
+++ src/com/java/linkedlist/SwapNodesInPairs.java
@@ -1,10 +1,31 @@
 package com.java.linkedlist;
 
+/**
+ * https://leetcode.com/problems/swap-nodes-in-pairs/
+ */
 public class SwapNodesInPairs {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public ListNode swapPairs(ListNode head) {
+		if(head == null) {
+			return head;
+		}
+		
+		ListNode dummyHead = new ListNode(-1);
+		dummyHead.next = head;
+		
+		ListNode resultNode = dummyHead;
+		//1->2->3->4
+		//0->1->2->3->4
+		while(head != null && head.next != null) {
+			ListNode temp = head.next;
+			head.next = temp.next;
+			head = head.next;
+			
+			ListNode temp2 = dummyHead.next;
+			temp.next = dummyHead.next;
+			dummyHead.next = temp;
+			dummyHead = temp2;
+		}
+		return resultNode.next;
 	}
-
 }
