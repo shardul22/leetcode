fc3669e2f0a34994b77bf630bd3594ae148d60cf
Yogesh Patwardhan <patwardhan.yogesh16@gmail.com>
2016-11-27 21:17:15 -0600

    Problem-1 : Two Sum

--- src/com/java/array/TwoSum.java
+++ src/com/java/array/TwoSum.java
@@ -8,9 +8,26 @@ import java.util.Map;
  */
 public class TwoSum {
 
+    /**
+     * The function below calculates two sum and returns result array containing numbers
+     * @param nums number array to find sum
+     * @param target target sum
+     * @return result array containing elemtents
+     */
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numberMap = new HashMap<>();
+
+        /*
+         * Steps to solve problem :-
+         *
+         * 1) Check the boundry conditions
+         * 2) As the input array contains exactly on pair, add required sum and index i pair to map.
+         * 3) If the required sum is present in the map then return result array with the values.
+         */
         int[] resultArray = new int[2];
+        if(nums == null || nums.length < 2)
+            return resultArray;
+        
+        Map<Integer, Integer> numberMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (numberMap.containsKey(nums[i])) {
                 resultArray[0] = numberMap.get(nums[i]);
@@ -20,5 +37,4 @@ public class TwoSum {
         }
         return resultArray;
     }
-
 }
