eec064aeb9e68b8d85f1ac6aa0ebb7d4cc72275b
Yogesh Patwardhan <patwardhan.yogesh16@gmail.com>
2016-11-27 20:54:06 -0600

    Problem-88 : Merge Sorted Array

--- src/com/java/array/MergeSortedArray.java
+++ src/com/java/array/MergeSortedArray.java
@@ -5,7 +5,26 @@ package com.java.array;
  */
 public class MergeSortedArray {
 
+    /**
+     * The function merges two sorted array in result array (larger array in this case).
+     * @param nums1 larger array
+     * @param m length of the larger array
+     * @param nums2 smaller array
+     * @param n length of smaller array
+     */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        /*
+         * Steps to solve problem :-
+         *
+         * 1) Start from the end of both of the array.
+         * 2) Compare the two elements of the array and store it at the end.
+         * 3) Decrement result index and store number index and continue further.
+         * 4) If the index of larger array reaches to zero then copy the rest of the elements
+         * of the array.
+         *
+         */
+
         // In this code i denotes number of elements in nums1 array.
         // j denotes number of elements in nums1 array.
         // k denotes length of result array which is nums1 in this case.
